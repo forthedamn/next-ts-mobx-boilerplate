32ac58ffdf6cc2d145a4238f80f24a1510462679
forthedamn <zhoguoxin@126.com>
2018-08-15 23:39:40 +0800

    style(shell): style

--- components/TodoItem/index.tsx
+++ components/TodoItem/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { ITodo, StatusEnum } from '@serv/todo';
 import { format } from '@utl/timer';
+import Link from 'next/link';
 
 import * as css from './index.css';
 
@@ -10,27 +11,29 @@ interface IProps extends ITodo {
 
 export default (props: IProps) => {
   return (
-    <div className={css.container}>
-      <div className={css.line}>
-        <div className={css.schema} title={props.schema}>
-          {props.schema}
-        </div>
-        <div>
-          {format(props.createTime)}
-        </div>
+    <Link href={{ pathname: '/detail', query: { id: props.id } }} >
+      <div className={css.container}>
+          <div className={css.line}>
+            <span className={css.schema} title={props.schema}>
+              {props.schema}
+            </span>
+            <span>
+              {format(props.createTime)}
+            </span>
+          </div>
+          <div className={css.line}>
+            <span className={css.content}>
+              {props.content}
+            </span>
+            <span>
+              <input
+                type="checkbox"
+                onChange={props.onChange}
+                checked={props.status === StatusEnum.close}
+              />
+            </span>
+          </div>
       </div>
-      <div className={css.line}>
-        <div className={css.content}>
-          {props.content}
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            onChange={props.onChange}
-            checked={props.status === StatusEnum.close}
-          />
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
